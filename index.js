5ed62f5fc24cc3b10ca01ab46bb32e1fcfa585bb
howmun <howmun@gmail.com>
2022-01-23 21:24:25 +0800

    added stripe api key

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const stripe = require('stripe')('sk_test_51KL5zGIpVBORLBeN5WMZXOUGUPc4LeqNmF8bdV9AlCNP4xBabkXQwV1M7IpbuBXwvL9e1WdtA41oZzK9ZA45xY25009CTzYFUt');
+
 app.get('/api', (req, res) => {
   // apiKey Store in header like X-Api-Key is better
   const apiKey = req.query.apiKey;
