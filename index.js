556e2a9b1091744d60276c641708e8b2d381274a
howmun <howmun@gmail.com>
2022-01-23 22:07:46 +0800

    - webhook from stripe half done

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 
 const stripe = require('stripe')('sk_test_51KL5zGIpVBORLBeN5WMZXOUGUPc4LeqNmF8bdV9AlCNP4xBabkXQwV1M7IpbuBXwvL9e1WdtA41oZzK9ZA45xY25009CTzYFUt');
 
+
+// Parsing rawBody required by stripe webhook
+app.use(
+  express.json({
+    verify: (req, res, buffer) => (req['rawBody'] = buffer),
+  })
+);
+
 // the serving api endpoint
 app.get('/api', (req, res) => {
   // apiKey Store in header like X-Api-Key is better
@@ -29,6 +37,86 @@ app.post('/checkout', async (req, res) => {
   });
 
   res.send(session);
+});
+
+// webhook for stripe to call
+app.post('/webhook', async(req,res)=>{
+  let data;
+  let eventType;
+  // Check if webhook signing is configured.
+  const webhookSecret = 'whsec_VoyJY1xrpGqNDvixkwc9QXsfLIMNoE4z';
+
+  if (webhookSecret) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers['stripe-signature'];
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        req['rawBody'],
+        signature,
+        webhookSecret
+      );
+    } catch (err) {
+      console.log(err);
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    // Extract the object from the event.
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+
+  switch (eventType) {
+    case 'checkout.session.completed':
+      console.log(data);
+      // Data included in the event object:
+      const customerId = data.object.customer;
+      const subscriptionId = data.object.subscription;
+
+      console.log(
+        `💰 Customer ${customerId} subscribed to plan ${subscriptionId}`
+      );
+
+      // Get the subscription. The first item is the plan the user subscribed to.
+      const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+      const itemId = subscription.items.data[0].id;
+
+      // Generate API key
+      const { apiKey, hashedAPIKey } = generateAPIKey();
+      console.log(`User's API Key: ${apiKey}`);
+      console.log(`Hashed API Key: ${hashedAPIKey}`);
+
+      // Store the API key in your database.
+      customers[customerId] = {
+        apikey: hashedAPIKey,
+        itemId,
+        active: true,
+      };
+      apiKeys[hashedAPIKey] = customerId;
+
+      break;
+    case 'invoice.paid':
+      // Continue to provision the subscription as payments continue to be made.
+      // Store the status in your database and check when a user accesses your service.
+      // This approach helps you avoid hitting rate limits.
+      break;
+    case 'invoice.payment_failed':
+      // The payment failed or the customer does not have a valid payment method.
+      // The subscription becomes past_due. Notify your customer and send them to the
+      // customer portal to update their payment information.
+      break;
+    default:
+    // Unhandled event type
+  }
+
+  res.sendStatus(200);
+
 });
 
 
